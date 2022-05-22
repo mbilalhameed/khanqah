f2efde7753007d364bcb05d561bdbf3b6bb9102a
mbilalhameed <mbilal.hameed@outlook.com>
2022-05-22 15:05:42 +0500

    feat: token based auth for users with password reset using DJOSER, JWT, mailtrap

--- khanqah/khanqah/settings/development.py
+++ khanqah/khanqah/settings/development.py
@@ -1,4 +1,16 @@
+from django.conf.global_settings import EMAIL_PORT
+
 from .base import *
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'info@khanqah-estate.com'
+Domain = env('DOMAIN')
+SITE_NAME = 'Khanqah'
 
 DATABASES = {
     'default': {
