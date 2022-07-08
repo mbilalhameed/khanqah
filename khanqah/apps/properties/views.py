8bcd7436889007f165625c404485bb24f493e3e6
mbilalhameed <mbilal.hameed@outlook.com>
2022-07-09 02:40:13 +0500

    added urls for properties API views

--- khanqah/apps/properties/views.py
+++ khanqah/apps/properties/views.py
@@ -129,4 +129,131 @@ def update_property_api_view(request, slug):
 @api_view(["POST"])
 @permission_classes([permissions.IsAuthenticated])
 def create_property_api_view(request):
-    pass
+    user = request.user
+    data = request.data
+    data["user"] = request.user.pkid
+    serializer = PropertyCreateSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        logger.info(f"Property {serializer.data.get('title')} created by {user.username}")
+
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+@permission_classes([permissions.IsAuthenticated])
+def delete_property_api_view(request, slug):
+    try:
+        property = Property.objects.get(slug=slug)
+    except Property.DoesNotExist:
+        raise PropertyNotFound
+
+    user = request.user
+    if property.user != user:
+        return Response(
+            {
+                "error": "You can't update or edit a property that doesn't belong to you"
+            },
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    if request.method == "DELETE":
+        delete_operation = property.delete()
+        data = {}
+
+        if delete_operation:
+            data["success"] = "Deletion was successful"
+        else:
+            data["failure"] = "Deletion Failed"
+
+        return Response(data=data)
+
+
+@api_view(["POST"])
+def upload_property_image_api_view(request):
+    data = request.data
+
+    property_id = data["property_id"]
+    property = Property.objects.get(id=property_id)
+    property.cover_photo = request.FILES.get("cover_photo")
+    property.photo1 = request.FILES.get("photo1")
+    property.photo2 = request.FILES.get("photo2")
+    property.photo3 = request.FILES.get("photo3")
+    property.photo4 = request.FILES.get("photo4")
+    property.save()
+    return Response("Image(s) uploaded successfully")
+
+
+class PropertySearchAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = PropertyCreateSerializer
+
+    def post(self, request):
+        queryset = Property.objects.filter(punlished_status=True)
+        data = self.request.data
+
+        advert_type = data["advert_type"]
+        queryset = queryset.filter(advert_type__iexact=advert_type)
+
+        property_type = data["property_type"]
+        queryset = queryset.filter(property_type__iexact=property_type)
+
+        price = data["price"]
+        if price == "$0+":
+            price = 0
+        elif price == "$50,000+":
+            price = 50000
+        elif price == "$100,000+":
+            price = 100000
+        elif price == "$200,000+":
+            price = 200000
+        elif price == "$400,000+":
+            price = 400000
+        elif price == "$600,000+":
+            price = 600000
+        elif price == "Any":
+            price = -1
+
+        if price != -1:
+            queryset = queryset.filter(price__gte=price)
+
+        bedrooms = data["bedrooms"]
+        if bedrooms == "0+":
+            bedrooms = 0
+        if bedrooms == "1+":
+            bedrooms = 1
+        if bedrooms == "2+":
+            bedrooms = 2
+        if bedrooms == "3+":
+            bedrooms = 3
+        if bedrooms == "4+":
+            bedrooms = 4
+        if bedrooms == "5+":
+            bedrooms = 5
+
+        queryset = queryset.filter(bedrooms__gte=bedrooms)
+
+        bathrooms = data["bathrooms"]
+        if bathrooms == "0+":
+            bathrooms = 0
+        if bathrooms == "1+":
+            bathrooms = 1
+        if bathrooms == "2+":
+            bathrooms = 2
+        if bathrooms == "3+":
+            bathrooms = 3
+        if bathrooms == "4+":
+            bathrooms = 4
+        if bathrooms == "5+":
+            bathrooms = 5
+
+        queryset = queryset.filter(bathrooms__get=bathrooms)
+
+        catch_phrase = data["catch_phrase"]
+        queryset = queryset.filter(catch_phrase__icontains=catch_phrase)
+
+        serializer = PropertySerializer(queryset, many=True)
+        return Response(serializer.data)
