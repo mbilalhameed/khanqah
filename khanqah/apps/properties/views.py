9c8524784fba725a695b0a9f4ae5ca0afa00075d
mbilalhameed <mbilal.hameed@outlook.com>
2022-07-08 23:59:00 +0500

    Created API views for properties

--- khanqah/apps/properties/views.py
+++ khanqah/apps/properties/views.py
@@ -1,3 +1,132 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .exceptions import PropertyNotFound
+from .models import Property, PropertyViews
+from .pagination import PropertyPagination
+from .serializers import (
+    PropertySerializer, PropertyCreateSerializer, PropertyViewSerializer
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+class PropertyFilter(django_filters.FilterSet):
+
+    advert_type = django_filters.ChoiceFilter(
+        field_name="advert_type", lookup_expr="iexact"
+    )
+
+    property_type = django_filters.CharFilter(
+        field_name="property_type", lookup_expr="iexact"
+    )
+
+    price = django_filters.NumberFilter()
+    price__gt = django_filters.NumberFilter(
+        field_name="price", lookup_expr="gt"
+    )
+    price__lt = django_filters.NumberFilter(
+        field_name="price", lookup_expr="lt"
+    )
+
+    class Meta:
+        model = Property
+        fields = ["advert_type", "property_type", "price"]
+
+
+class ListAllPropertiesAPIView(generics.ListAPIView):
+    serializer_class = PropertySerializer
+    queryset = Property.objects.all().order_by("-created_at")
+    pagination_class = PropertyPagination
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_class = PropertyFilter
+    search_fields = ["country", "city"]
+    ordering_fields = ["created_at"]
+
+
+class ListAgentsPropertyAPIView(generics.ListAPIView):
+    serializer_class = PropertySerializer
+    pagination_class = PropertyPagination
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_class = PropertyFilter
+    search_fields = ["country", "city"]
+    ordering_fields = ["created_at"]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Property.objects.filter(user=user).order_by("-created_at")
+        return queryset
+
+
+class PropertyViewsAPIView(generics.ListAPIView):
+    serializer_class = PropertyViewSerializer
+    queryset = PropertyViews.objects.all()
+
+
+class PropertyDetailAPIView(APIView):
+
+    def get(self, request, slug):
+        property = Property.objects.get(slug=slug)
+
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        if not PropertyViews.objects.filter(property=property, ip=ip).exists():
+            property .views += 1
+            property.save()
+
+        serializer = PropertySerializer(property, context={"request": request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["PUT"])
+@permission_classes([permissions.IsAuthenticated])
+def update_property_api_view(request, slug):
+    try:
+        property = Property.objects.get(slug=slug)
+    except Property.DoesNotExist:
+        raise PropertyNotFound
+
+    user = request.user
+    if property.user != user:
+        return Response(
+            {
+                "error": "You can't update or edit a property that doesn't belong to you"
+            },
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    if request.method == "PUT":
+        data = request.data
+        serializer = PropertySerializer(property, data, many=False)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+
+@api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated])
+def create_property_api_view(request):
+    pass
